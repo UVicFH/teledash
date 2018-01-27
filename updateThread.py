97deb284754e1d0b9eccb7ad1474711d003005a2
Chad McColm <chadjmccolm@gmail.com>
2018-01-27 15:58:27 -0800

    Added comments to main and updateThread

--- updateThread.py
+++ updateThread.py
@@ -1,12 +1,18 @@
-from PyQt5.QtCore import QThread, pyqtSignal
-from random import randint
+# import python system modules
 import time
 import math
+from random import randint
 
+# import relevant PyQT5 modules
+from PyQt5.QtCore import QThread, pyqtSignal
+
+# import the paho-mqtt module to communicate with the MQTT server
 import paho.mqtt.client as mqtt
 
+#defines the updateThread class and all functions
 class UpdateThread(QThread):
 	
+	# provides signals that will push to functions in wherever the updateThead object is created
 	gear = pyqtSignal(str)
 	speed = pyqtSignal(str)
 	rpm = pyqtSignal(int)
@@ -15,6 +21,7 @@ class UpdateThread(QThread):
 	chargePercent = pyqtSignal(int)
 	fuelPercent = pyqtSignal(int)
 
+	# initialize the updateThread
 	def __init__(self,parent=None):  
 
 		super(UpdateThread,self).__init__(parent)  
@@ -29,6 +36,7 @@ class UpdateThread(QThread):
 		self.exiting = True
 		self.terminate()
 
+	# set run behaviour to the checkForUpdates thread
 	def run(self):
 
 		while not self.exiting:
@@ -37,50 +45,71 @@ class UpdateThread(QThread):
 
 		return 
 
+	# defines the check for updates thread
 	def checkForUpdates(self):
 
-#		broker_address="192.168.1.65"
-#		client =mqtt.Client("Mosquitto Teleserver")
-#		try:
-#			client.connect(broker_address)
-#		except ConnectionRefusedError:
-#			self.statusText.emit(str("SRVR"))
-#			self.statusColor.emit(str("red"))
+		# connect to the MQTT client
+		broker_address="127.0.0.1"
+		client=mqtt.Client("Mosquitto Teleserver")
+		try:
+			client.connect(broker_address)
+		except ConnectionRefusedError:
+			self.statusText.emit(str("SRVR"))
+			self.statusColor.emit(str("red"))
 
-		time.sleep(5)
+		# pause before animating
+		time.sleep(2)
 
+		# reset everything to start
 		self.statusText.emit(str("OK"))
 		self.statusColor.emit(str("#07DD07"))
 		self.fuelPercent.emit(int(100))
 		self.chargePercent.emit(int(100))
 
+		# change rpm, speed, and gear through a range
 		x = -150
-		while x < 150:
+		while x <= 150:
+
+			# push data to UI
 			self.rpm.emit(int(x))
 			self.speed.emit(str(math.ceil((x+150)/3)))
 			self.gear.emit(str(math.ceil((x+150)/60)))
-#			client.publish("car/rpm",x)
+
+			# push data to MQTT server
+			client.publish("car/rpm",x)
+			client.publish("car/speed",math.ceil((x+150)/3))
+			client.publish("car/gear",math.ceil((x+150)/60))
+
+			# slow down animation execution
 			time.sleep(.02)
 			x+=1
 
+		#change fuel percent through a range, send to UI, and push to MQTT
 		x = 100
-		while x > 25:
+		while x >= 25:
 			self.fuelPercent.emit(int(x))
+			client.publish("car/fuelPercent",x)
 			time.sleep(.05)
 			x-=1
 
+		# demonstrate the low fuel status
 		self.statusText.emit(str("FUEL"))
 		self.statusColor.emit(str("red"))
 
+		# change charge percent through a range, send to UI, and push to MQTT
 		x = 100
-		while x > 25:
+		while x >= 25:
 			self.chargePercent.emit(int(x))
+			client.publish("car/chargePercent",x)
 			time.sleep(.05)
 			x-=1
 
+		# demonstrate the low battery status
 		self.statusText.emit(str("BATT"))
-		time.sleep(1)
 
+		# wait and then demonstrate a hot engine warning
+		time.sleep(1)
 		self.statusText.emit(str("HOT"))
 
-		time.sleep(5)
+		# wait before looping again
+		time.sleep(2)
