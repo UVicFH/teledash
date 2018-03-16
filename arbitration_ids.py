67592c037143bba3fbe481055faf7d4bb9e828f7
crocanura <lennonwotzke@hotmail.com>
2018-03-15 23:02:30 -0700

    ui responding to can

---
+++ arbitration_ids.py
@@ -0,0 +1,6 @@
+rpm = 1512
+groundspeed  = 1513
+speed = groundspeed
+gear = 1514
+fuel = 1515
+charge = 1516
--- updateThread.py
+++ updateThread.py
@@ -16,7 +16,14 @@ import can
 # requires interface be set up before opening as detailed here (done on Pi already): http://skpang.co.uk/blog/archives/1220
 # you may need to "bring up" the interface first with: sudo /sbin/ip link set can0 up type can bitrate 500000
 # ^ not yet tested without doing this - if required will need to be automated
+import os
+os.system('sudo ./set_up_can.sh')
+print("If RETLINK answered 'Device or resource busy' don't worry.")
 bus=can.interface.Bus(channel='can0', bustype='socketcan_native')
+
+import arbitration_ids
+
+MSTIMEOUT = 100 # in milliseconds
 
 class UpdateThread(QThread):
 
@@ -52,23 +59,42 @@ class UpdateThread(QThread):
 		return
 
 	def checkForUpdates(self):
+		
+		# recieve a message on the bus and timeout after MSTIMEOUT ms, in which case the gui will show a timeout
+		message = bus.recv(MSTIMEOUT/1000)
 
-		# set the statusText to ok and green
-		self.statusText.emit(str("OK"))
-		self.statusColor.emit(str("#07DD07"))
+		if message is None: # bus timed out
+			# set the statusText to TIMED OUT and red
+			self.statusColor.emit("#DD0707")
+			self.statusText.emit("T/O")
+			return None
+		else:
+			# set the statusText to ok and green
+			self.statusText.emit(str("OK"))
+			self.statusColor.emit(str("#07DD07"))
 
-		# set the fuel and charge to 100%
-		self.fuelPercent.emit(int(100))
-		self.chargePercent.emit(int(100))
+		if message.arbitration_id == arbitration_ids.rpm:
 
-		# recieve a message on the bus and timeout after 1ms (note if it recieves nothign it'll return a message of "none" not sure how to handle that as right now it breaks the code)
-		message = bus.recv(1)
-		if(message.arbitration_id == 1512):
-
-			# if the id of the message is 1512 it is rpm which is sent as two bits which needs to be combined again after
+			# rpm is sent as two bits which needs to be combined again after
 			rpm = message.data[0]<<8 | message.data[0]
 
 			# rpmAngle is what is sent to the UI current which is from -150 to 150 corresponding to 0 to 12000
 			rpmAngle = int(rpm/40.0-150.0)
 			self.rpm.emit(rpmAngle)
+
+		# the rest of these are strightforward
+
+		elif message.arbitration_id == arbitration_ids.speed:
+			self.speed.emit(str(message.data[0])) # speed is is a string
+
+		elif message.arbitration_id == arbitration_ids.gear:
+			self.gear.emit(str(message.data[0])) # gear is a string
+
+		elif message.arbitration_id == arbitration_ids.charge:
+			self.chargePercent.emit(message.data[0]) # chargePercent is an integer
+		
+		elif message.arbitration_id == arbitration_ids.fuel:
+			self.fuelPercent.emit(message.data[0]) # fuelPercent is an integer
+		
+		
 
