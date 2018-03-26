b262ddc1fea5f9b3075afefa60b6d21752cd79c4
Chad McColm <chadjmccolm@gmail.com>
2018-03-26 02:00:50 -0700

    Changed Output to Reflect Reality
    Now that I have the real CAN data layout I've changed the outputs of the program to match what the Raspberry Pi will see on the car.

--- Arduino_CAN_Sender/Arduino_CAN_Sender.ino
+++ Arduino_CAN_Sender/Arduino_CAN_Sender.ino
@@ -47,25 +47,14 @@ void loop() {
   fuel = 7/760.0*rpm+55.0/19;
   charge = -1/80.0*rpm+130;
 
-  byte sendrpm[2] = {rpm>>8 & 0xFF, rpm & 0xFF};
+  byte id_1520[8] = {0, 0, 0, 0, 0, 0, rpm>>8, rpm&0xFF};
+  byte id_257[8] = {0, 0, 0, 0, groundspeed, charge, gear&0xF, 0};
+  byte id_258[8] = {0, 0, 0, 0, fuel, 0, 0, 0};
 
-  Serial.print("RPM:");
-  Serial.print(sendrpm[0]<<8 | sendrpm[1]);
-  Serial.print("\tSpeed:");
-  Serial.print(groundspeed);
-  Serial.print("\tGear:");
-  Serial.print(gear);
-  Serial.print("\tFuel:");
-  Serial.print(fuel);
-  Serial.print("\tCharge:");
-  Serial.println(charge);
-
-  // Send the message over CAN
-  CAN.sendMsgBuf(1512, 0, 2, sendrpm);
-  CAN.sendMsgBuf(1513, 0, 1, &groundspeed);
-  CAN.sendMsgBuf(1514, 0, 1, &gear);
-  CAN.sendMsgBuf(1515, 0, 1, &fuel);
-  CAN.sendMsgBuf(1516, 0, 1, &charge);
+  // Send the message over CAN to the same places they're sent on the car. See https://docs.google.com/spreadsheets/d/1sYD5FM78nsb_uLSUSBNHb7dAY1rldME3raeu9eHfI0A/edit?usp=sharing
+  CAN.sendMsgBuf(1520, 0, 8, id_1520);
+  CAN.sendMsgBuf(257, 0, 8, id_257);
+  CAN.sendMsgBuf(258, 0, 8, id_258);
   
   // Delay before sending the next signal
   delay(100);
