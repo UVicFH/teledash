3a499371baf48e00ea880d8d34da4782f4598855
Chad McColm <chadjmccolm@gmail.com>
2018-04-01 16:59:32 -0700

    coolant temp -> int

--- updateThread.py
+++ updateThread.py
@@ -195,5 +195,5 @@ class UpdateThread(QThread):
 			self.fuelPercent.emit(message.data[4]) # fuelPercent is an integer
 
 		elif message.arbitration_id == arbitration_ids.coolant:
-			self.statusText.emit(str(int(message.data[6]<<8 | message.data[7])/10.0) # status text needs to come out as string
+			self.statusText.emit(str(int(message.data[6]<<8 | message.data[7])/10) # status text needs to come out as string
 
